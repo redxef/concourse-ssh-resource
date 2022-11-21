c9d4d151acef59208f11cbfe40524c04c898d028
redxef <federanko.manuel@gmail.com>
2022-11-21 23:59:23 +0100

    Allow writing of logfiles instead of all to stderr.

--- src/scripts/put_files.sh
+++ src/scripts/put_files.sh
@@ -8,13 +8,35 @@ directory="${DIRECTORY}"
 workdir="${WORKDIR}"
 # shellcheck disable=SC2153
 extract="${EXTRACT}"
+# shellcheck disable=SC2153
+stderr_file="${STDERR_FILE}"
+# shellcheck disable=SC2153
+stdout_file="${STDOUT_FILE}"
+
 (
-    if "$extract"; then
-        cd "$directory"
-        tar x
-    fi
+  if "$extract"; then
+    cd "$directory"
+    tar x
+  fi
 )
 (
-    cd "$workdir"
-    ${COMMAND} >&2
+  cd "$workdir"
+  [ "$stderr_file" != "-" ] && truncate -s 0 "$stderr_file"
+  [ "$stdout_file" != "-" ] && truncate -s 0 "$stdout_file"
+
+  # don't write anything to stdout, since that would get appended to the tar file
+  if [ "$stderr_file" = "-" ] && [ "$stdout_file" = "-" ]; then
+    # redirect stdout to stderr
+    ${COMMAND} 1>&2
+  elif [ "$stderr_file" != "-" ] && [ "$stdout_file" = "-" ]; then
+    # stderr to file, stdout to stderr
+    ( ${COMMAND} 2>"$stderr_file" ) 1>&2
+
+  elif [ "$stderr_file" = "-" ] && [ "$stdout_file" != "-" ]; then
+    # stdout to file
+    ${COMMAND} 1>"$stdout_file"
+  else
+    # both files specified
+    ${COMMAND} 1>"$stdout_file" 2>"$stderr_file"
+  fi
 )
